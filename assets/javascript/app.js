c14fa6e6d181422cebca1fc20a265ba5ad5a1424
emilypo89 <emilypo89@gmail.com>
2017-06-06 20:47:51 -0400

    firebase setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDXRI5N36xqsvuefGexJQiggCaX13bu6lU",
+  authDomain: "trainscheduler-cb5eb.firebaseapp.com",
+  databaseURL: "https://trainscheduler-cb5eb.firebaseio.com",
+  projectId: "trainscheduler-cb5eb",
+  storageBucket: "trainscheduler-cb5eb.appspot.com",
+  messagingSenderId: "956005095343"
+};
+firebase.initializeApp(config);
+
+var database = database.firebase();
